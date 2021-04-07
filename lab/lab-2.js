457c379b37a44d67efce25e7ebb7c278c28c2383
hieuflynotes <hieu@flynotes.co.uk>
2021-04-07 01:31:45 +0100

    add lab 2 & lab 3

---
+++ lab/lab-2.js
@@ -0,0 +1,20 @@
+// 1. Định nghĩa một mảng gồm các phần tử: 5,6,4,3,2,5, 7, 8, 9
+// 2. Thêm vào mảng thêm các số 5,9,1
+// 3. Xuất ra màn hình gía trị các số tại vị trí số 1,3,5 => kết quả là: 6,3,5 
+// 4. Cập nhật giá trị tại vị trí số 2 trong mảng thành 60
+// 5. Xuất ra độ dài của mảng 
+// 6. Xoá các phần tử ở vị trí 6,7,8 và 0,1,2 trong mảng 
+// 7. Xuất ra tất cả các số trong mảng theo cú pháp 
+//          - Giá trị tại vị trí thứ {i} là {a[i]}
+// 8. Viết hàm trả ra danh sách tất cả các số nhỏ hơn n và chia hết cho 6
+//      - danhSachChiaHetCho6(n):[]
+//      - Ví dụ: danhSachChiaHetCho6(15) sẽ trả ra một mảng [0,6,12]
+// 9. Viết hàm trả ra danh sách tất cả các số nhỏ hơn n và chia hết cho m
+//      - danhSachChiaHet(n,m):[]
+//      - Ví dụ: danhSachChiaHet(10,4) sẽ trả ra một mảng [0,4,8]
+// 10. Viết hàm cộng tất c ả các phần tử trong mảng với n 
+//      - congMang(arr, n): []
+//      - Đầu vào là một mảng và n 
+//      - Đầu ra là một mảng mà tất cả các số được cộng với n
+//      - Ví dụ: arr = [1,2,3]; n=3
+// - Kết quả trả ra của hàm: [4,5,6]
---
+++ lab/lab-3.js
@@ -0,0 +1,17 @@
+// 1. Viết hàm xuất ra tin nhắn n lần: xuatTinNhan(noiDung, soLan): void
+//      Ví dụ: khi gọi hàm xuatTinNhan("xin chào", 10) thì sẽ xuất ra màn hình 10 lần từ xin chào
+// 2. Viết hàm tính tổng tất cả các số nhỏ hơn n: tinhTong(n)
+// 3. Viết hàm tính tính tổng tất cả những số nhỏ hơn n và chia hết cho cả 2; 3
+//      - tinhTongChiaHetCho2Va3(n): number
+// 4. Viết hàm tính tính tổng tất cả những số nhỏ hơn n và chia hết cho cả a; b
+//      - tinhTongChiaHet(n,a,b): number
+// 5. Viết hàm tính tổng n số lẻ nhỏ nhất
+//      - tinhTongSoLe(n)
+//      Ví dụ: tinhTongSoLe(5) = 1+3+5+7+9
+// 6. Viết hàm tính tổng n số nhỏ nhất chia hết cho cả 2 và 3
+//      tinhTongNSoNhoNhatChiaHetCho2Va3(n)
+// 7. Viết hàm tính tổng n số nhỏ nhất chia hết cho cả a và b
+//      - tinhTongNSoNhoNhatChiaHet(n,a,b)
+// 8. Viết hàm kiểm tra xem một số có phải số nguyên tố không. Nếu đúng là số nguyên tố thì trả ra true, nếu không thì trả ra false
+//      - isPrime(n): boolean 
+// 9. Viết hàm xuất ra n số nguyên tô nhỏ nhất 
