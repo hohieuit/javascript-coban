9ba39b39c180fe3bae17010f1d6b00d0719f28c8
hieuflynotes <hieu@flynotes.co.uk>
2021-04-08 07:57:11 +0100

    add lab 6

---
+++ lab/lab-6.js
@@ -0,0 +1,9 @@
+// 1. Tạo form tạo sản pẩhm gồm các trường: tên; gia; hãng và button thêm sản phẩm
+// 2. Viết hàm lắng nghe sự kiện onClick thêm sản phảm
+// 3. Truy cập node input đẻ lấy được dữ liệu: tên; gia; hãng
+// 4. Tạo đối tượng spMoi bao gồm 3 thông tin trên
+// 5. Lấy mảng đối tượng dưới local lên. Nếu chưa tồn tại dứoi local, thì khỏi tạo môtj mảng mới
+// 6. Thêm spMoi vào mảng đã lấy ở bước (5)
+// 7. LƯu lại mảng sau khi đã thêm sp xuống local 
+// ===========
+// 8. Ở màn hình danh sách sản phẩm đã làm trong lab 5, sẽ load dữ liệu từ local lên 
