582e73e2dd86db2b847040f05d353278693b70ae
hieuflynotes <hieu@flynotes.co.uk>
2021-04-10 01:25:42 +0100

    update lab 6

--- lab/lab-6.js
+++ lab/lab-6.js
@@ -1,4 +1,4 @@
-// 1. Tạo form tạo sản pẩhm gồm các trường: tên; gia; hãng và button thêm sản phẩm
+// 1. Tạo form tạo sản pẩhm gồm các trường: hình ảnh; tên; gia; hãng và button thêm sản phẩm
 // 2. Viết hàm lắng nghe sự kiện onClick thêm sản phảm
 // 3. Truy cập node input đẻ lấy được dữ liệu: tên; gia; hãng
 // 4. Tạo đối tượng spMoi bao gồm 3 thông tin trên
@@ -7,3 +7,7 @@
 // 7. LƯu lại mảng sau khi đã thêm sp xuống local 
 // ===========
 // 8. Ở màn hình danh sách sản phẩm đã làm trong lab 5, sẽ load dữ liệu từ local lên 
+// 9. Kiểm tra hợp lệ cho form
+    // - hinh ảnh, tên, giá không được để trống 
+    // - tên phải lớn hơn 5 kí tự & nhỏ hơn 50 kí tự 
+    // Nếu không hợp lệ thì sử dụng lệnh alert để thông báo cho người dùng biết là form chưa hợp lệ 
